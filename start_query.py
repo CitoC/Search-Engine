46e569114f4ab615a93d6c95bd510d69ba3dfea3
CitoC <56456342+CitoC@users.noreply.github.com>
2022-03-03 18:56:54 -0800

    minor update and added comments

--- start_query.py
+++ start_query.py
@@ -3,23 +3,28 @@ import time
 
 def main():
     test = Query()
-    doc_id = get_doc_url('documentsIDs.txt')
+    test.get_doc_url('documentsIDs.txt')
     
     while True:
-        test.get_doc_url('documentIDs.txt')
         
+        # get the start time (to determine running time of searches)
         t = time.process_time()
 
+        # prompt user to enter a search term
         user_input = test.get_input()
         if user_input == '-1': break
 
+        # read relevant data from index file from disk into memory
         test.retrieve_relevant_document('indexes/index.txt')
 
+        # calculate document rankings
         sorted_intersections = test.highest_tf_idf_scores()
 
+        # get top 5 search results
         for i in range(5):
-            print(doc_id[sorted_intersections[i]])
+            print(test.doc_id[sorted_intersections[i]])
 
+        # calculate elasped time for search
         elapsed_time = round((time.process_time() - t) * 1000)
         print('elapsed time:', elapsed_time, 'milliseconds\n')
         
